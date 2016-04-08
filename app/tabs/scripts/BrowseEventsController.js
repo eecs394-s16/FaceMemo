f4572bd8788c760d7804f579d1d3067893bd48b7
kevincheng96 <kevincheng96@hotmail.com>
2016-04-08 13:50:47 -0500

    scrapped factory and used localstorage instead

--- app/tabs/scripts/BrowseEventsController.js
+++ app/tabs/scripts/BrowseEventsController.js
@@ -1,23 +1,22 @@
 angular
   .module('tabs')
-  .controller("BrowseEventsController", ["$scope", "$firebaseArray", "eventFactory", function ($scope, $firebaseArray, eventFactory) {
+  .controller("BrowseEventsController", ["$scope", "$firebaseArray", "supersonic", function ($scope, $firebaseArray, supersonic) {
       var ref = new Firebase("https://scorching-fire-12.firebaseio.com/events");
       //download the data into a local object
       $scope.events = $firebaseArray(ref);
 
-      $scope.event = eventFactory.getEvent();
-
       //pass data into the eventInformation view specifying which event's information to show
       $scope.saveClickedEvent = function(e) {
-      	var clickedEvent = {
+      	var message = {
       		attendees: e.attendees,
       		date: e.date, 
       		image: e.image,
       		location: e.location,
       		name: e.name,
       		time: e.time,
+          description: e.description
       	}
-      	eventFactory.setEvent(clickedEvent);
-      	$scope.event = eventFactory.getEvent();
+        $scope.event = message;
+        window.localStorage.setItem("clicked_event", JSON.stringify(message));
       }
   }]);
--- app/tabs/scripts/EventInformationController.js
+++ app/tabs/scripts/EventInformationController.js
@@ -1,10 +1,8 @@
 angular
   .module('tabs')
-  .controller("EventInformationController", ["$scope", "$firebaseArray", "eventFactory", function ($scope, $firebaseArray, eventFactory) {
-  		$scope.event = eventFactory.getEvent();
-
-  		$scope.tester = function() {
-  			$scope.test1 = eventFactory.currentEvent;
-  		}
-  		//for some reason, factory variable is not being shared between two controllers
+  .controller("EventInformationController", ["$scope", "$firebaseArray", "supersonic", function ($scope, $firebaseArray, supersonic) {
+  		supersonic.ui.views.current.whenVisible(function() {
+		    var clicked_event = window.localStorage.getItem("clicked_event");
+		    $scope.event = JSON.parse(clicked_event);
+		});
   }]);
