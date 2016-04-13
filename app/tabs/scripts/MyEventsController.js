f0aa97676c6a903d2c5f3a2bcd8391860eb7e464
Yang Tang <Yang Tang>
2016-04-13 18:41:52 -0500

    fix: fix the dates

--- app/tabs/scripts/MyEventsController.js
+++ app/tabs/scripts/MyEventsController.js
@@ -5,10 +5,18 @@ angular
       // download the data into a local object
       $scope.events = $firebaseArray(ref);
 
+      $scope.events.$loaded().then(function() {
+        for (var i = 0; i < $scope.events.length; i++) {
+          var date = new Date($scope.events[i].date);
+          $scope.events[i].date = date;
+        }
+      });
+
+      
       //pass data into the eventInformation view specifying which event's information to show
       $scope.clickedEvent = function(e) {
       	// var list_of_attendees = e.attendees;
         window.localStorage.setItem("clickedEvent",JSON.stringify(e));
         // window.localStorage.setItem("list_of_attendees", JSON.stringify(list_of_attendees));
-      }
+      };
   }]);
