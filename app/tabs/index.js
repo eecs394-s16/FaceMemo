05d63e2d50d831e23b8c374c10eecce0afe66e77
Meg Grasse <meggrasse@gmail.com>
2016-04-18 11:17:37 -0500

    updates to linkedin

--- app/tabs/index.js
+++ app/tabs/index.js
@@ -3,18 +3,32 @@ angular.module('tabs', [
   'supersonic', 'common', 'firebase', 'satellizer'])
   .config(function($authProvider) {
     $authProvider.linkedin({
+    	url: '/auth/linkedin',
+  		authorizationEndpoint: 'https://www.linkedin.com/uas/oauth2/authorization',
+  		redirectUri: 'https://www.linkedin.com/uas/oauth2/authorization',
+		requiredUrlParams: ['state'],
+		scope: ['r_basicprofile'],
+		scopeDelimiter: ' ',
+		state: 'STATE',
+		type: '2.0',
+		popupOptions: { 
+			width: 527, 
+			height: 582 
+		},
       clientId: '77zxhwq7a7556j'
     });
-    $authProvider.httpInterceptor = function() { return true; },
-		$authProvider.withCredentials = true;
-		$authProvider.tokenRoot = null;
-		$authProvider.baseUrl = '/';
-		$authProvider.loginUrl = '/auth/login';
-		$authProvider.signupUrl = '/auth/signup';
-		$authProvider.unlinkUrl = '/auth/unlink/';
-		$authProvider.tokenName = 'token';
-		$authProvider.tokenPrefix = 'satellizer';
-		$authProvider.authHeader = 'Authorization';
-		$authProvider.authToken = 'Bearer';
-		$authProvider.storageType = 'localStorage';
+    $authProvider.httpInterceptor = function() { 
+    	return true; 
+    },
+	$authProvider.withCredentials = true;
+	$authProvider.tokenRoot = null;
+	$authProvider.baseUrl = '/';
+	$authProvider.loginUrl = '/auth/login';
+	$authProvider.signupUrl = '/auth/signup';
+	$authProvider.unlinkUrl = '/auth/unlink/';
+	$authProvider.tokenName = 'token';
+	$authProvider.tokenPrefix = 'satellizer';
+	$authProvider.authHeader = 'Authorization';
+	$authProvider.authToken = 'Bearer';
+	$authProvider.storageType = 'localStorage';
 });
