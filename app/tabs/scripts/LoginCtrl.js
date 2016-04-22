35cf08c1e60b990c736f49ef6465ff3aea220bb0
Meg Grasse <meggrasse@gmail.com>
2016-04-22 10:48:03 -0500

    linkedin data saved to firebase

--- app/tabs/scripts/LoginCtrl.js
+++ app/tabs/scripts/LoginCtrl.js
@@ -1,11 +1,8 @@
 angular
   .module('tabs')
   // .controller('LoginCtrl', function(store, $scope, $location, auth) {
-  .controller('LoginCtrl', function(supersonic, $scope, $location, store, auth) {
-    $scope.test = "Yang";
-    
+  .controller('LoginCtrl', function(supersonic, $scope, $location, store, auth) {    
     $scope.login = function() {
-      $scope.test = "Yang logging in";
       auth.signin({
         authParams: {
           scope: 'openid offline_access',
@@ -13,10 +10,19 @@ angular
         }
       }, function(profile, token, accessToken, state, refreshToken) {
         // Success callback
-        console.log("success!" + JSON.stringify(profile));
+        //console.log("success!" + JSON.stringify(profile));
+        var ref = new Firebase("https://scorching-fire-12.firebaseio.com");
+        var userRef = ref.child("users");
         store.set('profile', profile);
         store.set('token', token);
         store.set('refreshToken', refreshToken);
+        userRef.child(profile.user_id).set({
+          name: profile.name,
+          LinkedinURL: profile.publicProfileUrl,
+          image: profile.picture,
+          email: profile.email,
+          title: profile.headline
+        });
         $location.path('/');
         var view = new supersonic.ui.View("tabs#myEvents");
         supersonic.ui.layers.push(view);
@@ -27,3 +33,78 @@ angular
       });
     };
 });
+
+// angular
+//   .module('tabs')
+//   // .controller('LoginCtrl', function(store, $scope, $location, auth) {
+//   .controller('LoginCtrl', function(supersonic, $scope, $location, store, auth) {
+//     $scope.login = function() {
+//       auth.signin({
+//         authParams: {
+//           scope: 'openid offline_access',
+//           device: 'Mobile device'
+//         }
+//       }, function(profile, token, accessToken, state, refreshToken) {
+//         // Success callback
+//         //console.log("success!" + JSON.stringify(profile));
+//         var ref = new Firebase("https://scorching-fire-12.firebaseio.com/users");
+//         $scope.users = $firebaseArray(ref);
+//         store.set('profile', profile);
+//         store.set('token', token);
+//         store.set('refreshToken', refreshToken);
+//         $location.path('/');
+//         ref.once("value", function(snapshot) {
+//           var a = snapshot.child("email").exists();
+//           if a == true {
+//             alert("true");
+//           }
+//         }
+//         var view = new supersonic.ui.View("tabs#myEvents");
+//         supersonic.ui.layers.push(view);
+
+//       }, function() {
+//         // Error callback
+//         alert("error");
+//       });
+//     };
+// });
+//     }
+//   }
+
+// angular
+//   .module('tabs')
+//   .controller('SettingsController', function($scope, supersonic) {
+//     var ref = new Firebase("https://scorching-fire-12.firebaseio.com");
+//     $scope.submit = function() {
+//       $scope.test = $scope.role
+//       ref.createUser({
+//         email: $scope.email,
+//         password: $scope.password
+//       }, function(error, userData) {
+//         if (error) {
+//         switch (error.code) {
+//             case "EMAIL_TAKEN":
+//               alert("The new user account cannot be created because the email is already in use.");
+//               break;
+//             case "INVALID_EMAIL":
+//               alert("The specified email is not a valid email.");
+//               break;
+//             default:
+//               alert(error);
+//           }
+//       } else {
+//         var usersRef = ref.child("users");
+//         usersRef.push().set({
+//             email: $scope.email,
+//             name: $scope.name,
+//             company: $scope.company,
+//             position: $scope.position,
+//             linkedinurl: $scope.linkedin, 
+//             role: $scope.role,
+//             id: userData.uid
+//         });
+//         alert("Your account was successfully created")
+//       }
+//     });
+//     }
+// });
