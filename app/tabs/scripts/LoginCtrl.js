b15248ef5f56f843e4e015d6fb2de312575d8616
Meg Grasse <meggrasse@gmail.com>
2016-04-23 18:17:22 -0500

    buggy pictureurl code

--- app/tabs/scripts/LoginCtrl.js
+++ app/tabs/scripts/LoginCtrl.js
@@ -1,125 +1,72 @@
-angular
-  .module('tabs')
-  // .controller('LoginCtrl', function(store, $scope, $location, auth) {
-  .controller('LoginCtrl', function(supersonic, $scope, $location, store, auth) {    
-    $scope.login = function() {
-      auth.signin({
-        authParams: {
-          scope: 'openid offline_access',
-          device: 'Mobile device'
-        }
-      }, function(profile, token, accessToken, state, refreshToken) {
-        // Success callback
-        //console.log("success!" + JSON.stringify(profile));
-        store.set('profile', profile);
-        store.set('token', token);
-        store.set('refreshToken', refreshToken);
-        store.set('user_id', profile.user_id);
+var tabs = angular.module('tabs');
 
-        //Adding user to firebase if it doesn't exist already
-        var ref = new Firebase("https://scorching-fire-12.firebaseio.com/users");
-        ref.once("value", function(snapshot) {
-          if (snapshot.child(profile.user_id).exists()) {
-            ref.child(profile.user_id).update({
-              name: profile.name,
-              LinkedinURL: profile.publicProfileUrl,
-              image: profile.picture,
-              email: profile.email,
-              title: profile.headline
-            })
-          }
-          else {
-            ref.child(profile.user_id).set({
-              name: profile.name,
-              LinkedinURL: profile.publicProfileUrl,
-              image: profile.picture,
-              email: profile.email,
-              title: profile.headline
-            });
-          }
-        }) 
-        $location.path('/');
-        var view = new supersonic.ui.View("tabs#myEvents");
-        supersonic.ui.layers.push(view);
-
-      }, function() {
-        // Error callback
-        alert("error");
-      });
-    };
+tabs.controller('LoginCtrl', function(supersonic, $scope, $location, $http, store, auth) {    
+	$scope.login = function() {
+		auth.signin({
+			authParams: {
+				scope: 'openid offline_access',
+				device: 'Mobile device'
+			}
+		},
+		function(profile, token, accessToken, state, refreshToken) {
+			alert("here1");
+			alert(profile.identities[0].access_token);
+			var access_token = profile.identities[0].access_token
+			var img_url = "https://api.linkedin.com/v1/people/~/picture-urls::(original)?oauth2_access_token=" + access_token
+			function loadPicture() {
+				alert("in loadpicture()")
+				var x2js = new X2JS();
+				$http.get(img_url).then(function(data) {
+				// pictureFactory.getPicture().then(function(data) {
+					alert("in http get")
+					urls = x2js.xml_str2json(data);
+					alert("urls.picture-urls.picture-url")
+					store.set('pictureUrl', urls.picture-urls.picture-url);
+				},
+				function(error) {
+					alert(JSON.stringify(error));
+				});
+			}
+			alert("here5")
+			loadPicture();
+			store.set('profile', profile);
+			store.set('token', token);
+			store.set('refreshToken', refreshToken);
+			store.set('user_id', profile.user_id);
+			var ref = new Firebase("https://scorching-fire-12.firebaseio.com/users");
+			alert("here")
+			ref.once("value", function(snapshot) {
+				if (snapshot.child(profile.user_id).exists()) {
+					ref.child(profile.user_id).update({
+						name: profile.name,
+						LinkedinURL: profile.publicProfileUrl,
+						image: store.get('pictureUrl'),
+						email: profile.email,
+						title: profile.headline
+					})
+			  	}
+				else {
+					ref.child(profile.user_id).set({
+						name: profile.name,
+						LinkedinURL: profile.publicProfileUrl,
+						image: store.get('pictureUrl'),
+						email: profile.email,
+						title: profile.headline
+					});
+			  	}
+			}) 
+			$location.path('/');
+			var view = new supersonic.ui.View("tabs#myEvents");
+			supersonic.ui.layers.push(view);
+		})
+	};
 });
 
-// angular
-//   .module('tabs')
-//   // .controller('LoginCtrl', function(store, $scope, $location, auth) {
-//   .controller('LoginCtrl', function(supersonic, $scope, $location, store, auth) {
-//     $scope.login = function() {
-//       auth.signin({
-//         authParams: {
-//           scope: 'openid offline_access',
-//           device: 'Mobile device'
-//         }
-//       }, function(profile, token, accessToken, state, refreshToken) {
-//         // Success callback
-//         //console.log("success!" + JSON.stringify(profile));
-//         var ref = new Firebase("https://scorching-fire-12.firebaseio.com/users");
-//         $scope.users = $firebaseArray(ref);
-//         store.set('profile', profile);
-//         store.set('token', token);
-//         store.set('refreshToken', refreshToken);
-//         $location.path('/');
-//         ref.once("value", function(snapshot) {
-//           var a = snapshot.child("email").exists();
-//           if a == true {
-//             alert("true");
-//           }
-//         }
-//         var view = new supersonic.ui.View("tabs#myEvents");
-//         supersonic.ui.layers.push(view);
-
-//       }, function() {
-//         // Error callback
-//         alert("error");
-//       });
-//     };
-// });
-//     }
-//   }
-
-// angular
-//   .module('tabs')
-//   .controller('SettingsController', function($scope, supersonic) {
-//     var ref = new Firebase("https://scorching-fire-12.firebaseio.com");
-//     $scope.submit = function() {
-//       $scope.test = $scope.role
-//       ref.createUser({
-//         email: $scope.email,
-//         password: $scope.password
-//       }, function(error, userData) {
-//         if (error) {
-//         switch (error.code) {
-//             case "EMAIL_TAKEN":
-//               alert("The new user account cannot be created because the email is already in use.");
-//               break;
-//             case "INVALID_EMAIL":
-//               alert("The specified email is not a valid email.");
-//               break;
-//             default:
-//               alert(error);
-//           }
-//       } else {
-//         var usersRef = ref.child("users");
-//         usersRef.push().set({
-//             email: $scope.email,
-//             name: $scope.name,
-//             company: $scope.company,
-//             position: $scope.position,
-//             linkedinurl: $scope.linkedin, 
-//             role: $scope.role,
-//             id: userData.uid
-//         });
-//         alert("Your account was successfully created")
-//       }
-//     });
-//     }
+// tabs.factory('pictureFactory',function($http) {
+// 	var factory = [];
+// 	factory.getPicture = function(){
+// 		alert("here11")
+// 		return $http.get("https://api.linkedin.com/v1/people/~/picture-urls::(original)?oauth2_access_token=AQW6UOS7QgqhvSdlLPXub8vBCUNXq8wZRWTR0IrgoVq_IpiXW_BtROr0ITkC7B4wUE2rWagW-uLEKKJveI44hqcWx9qfNK7CsVdBTpP8gvaz2ZY8hP6FyYF4QgWNPaeijT-i_53Fwrd8mQG9VIaHBKeARL_Xuz6sIZucSSw1U8XQuH47BGo");
+// 	}
+// 	return factory;
 // });
