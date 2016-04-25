5eff688464f4ee1217b8c18561bfdc62a2d7c812
[katiegeorge] <katherinegeorge2016@u.northwestern.edu>
2016-04-25 13:16:28 -0500

    adding logout functionality

--- app/tabs/scripts/ProfileController.js
+++ app/tabs/scripts/ProfileController.js
@@ -1,7 +1,17 @@
 angular
   .module('tabs')
-  .controller("ProfileController", function ($scope, supersonic, store) {
+  .controller("ProfileController", function ($scope, supersonic, store, updateLocalStorage) {
+      
+  		 // update localStorage when logged out
+    updateLocalStorage();
+
+    // load login page
+    function loadLogin() {
+      supersonic.ui.layers.pop();
+    };
+
       supersonic.ui.views.current.whenVisible(function() {
+
       	$scope.uid = store.get("uid");
       	var profile = store.get('profile');
       	// $scope.profile = profile;
@@ -13,5 +23,22 @@ angular
               title: profile.headline
       	}
       });
+
+      $scope.logout = function() {
+        alert("signed out");
+
+        //update all localStorage items
+        store.remove('profile');
+        store.remove('token');
+        store.remove('uid');
+        window.postMessage({
+          'profile': null,
+          'token': null,
+          'uid':null
+        });
+
+        auth.signout();
+        loadLogin();
+      };
 
   });
